be5c03acac085964ab683bb662bde000683d9376
Ubuntu <ubuntu@ip-172-31-26-110.ec2.internal>
2025-12-06 19:03:23 +0000

    Corregir install_lamp_frontend.sh en frontend

--- practica-1.8/scripts/install_lamp_frontend.sh
+++ practica-1.8/scripts/install_lamp_frontend.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-#-e:Finaliza el script cuando hay un error
-#-x: Muestra el comando por pantalla
-set -x
+# -e: Finaliza el script cuando hay un error
+# -x: Muestra el comando por pantalla
+set -ex
 
-#Actualiza los repositorios
-apt update
+# Actualiza los repositorios
+sudo apt update
 
-#Actualizamos los paquetes 
-apt upgrade -y
+# Actualizamos los paquetes 
+sudo apt upgrade -y
 
-#Instalamos el servidor web apache
-apt install apache2 -y
+# Instalamos el servidor web Apache
+sudo apt install -y apache2
 
-#Instalamos PHP
-apt install php libapache2-mod-php php-mysql -y
+# Instalamos PHP y módulo de MySQL
+sudo apt install -y php libapache2-mod-php php-mysql
 
-#Copiamos el archivo de configuración de Apache
-cp ../conf/000-default.conf /etc/apache2/sites-available/000-default.conf
+# Copiamos el archivo de configuración de Apache
+sudo cp ../conf/000-default.conf /etc/apache2/sites-available/000-default.conf
 
-#Habilitamos el módulo rewrite de Apache
-a2enmod rewrite
+# Habilitamos el módulo rewrite de Apache
+sudo a2enmod rewrite
 
-#Reiniciar el servicio de Apache
-systemctl restart apache2
+# Reiniciar el servicio de Apache
+sudo systemctl restart apache2
 
-#Modificamos el propietario y el grupo de /var/www/html
-chown -R www-data:www-data /var/www/html
+# Modificamos el propietario y el grupo de /var/www/html
+sudo chown -R www-data:www-data /var/www/html
